a057e4ec84da64f619aa7e21f1504def410b71b3
BealBC <beal.bc@gmail.com>
2018-06-15 10:03:14 -0700

    Update view.php

--- application/views/pics/view.php
+++ application/views/pics/view.php
@@ -2,7 +2,6 @@
 //application/views/pics/view.php
 $this->load->view($this->config->item('theme') . 'header'); 
 echo '<br />';
-//echo "<div.box style='float: left; '>";
 echo "<div.box style='width: 100%;display:flex;flex-flow:row wrap;'>";
 foreach ($pics as $pic) 
 {
@@ -10,7 +9,6 @@ foreach ($pics as $pic)
     echo "<div>";
     $size = 'm';
     $photo_url = 'http://farm'. $pic->farm . '.staticflickr.com/' . $pic->server . '/' . $pic->id . '_' . $pic->secret . '_' . $size . '.jpg';
-    //echo  '</br>';
     echo "<img title='" . $pic->title . "' src='" . $photo_url . "' />";
     echo '</div>';
     echo '<div>';
@@ -19,4 +17,4 @@ foreach ($pics as $pic)
     echo '</div>';
 }
 echo '</div>';    
-$this->load->view($this->config->item('theme') . 'footer'); 
+$this->load->view($this->config->item('theme') . 'footer');
